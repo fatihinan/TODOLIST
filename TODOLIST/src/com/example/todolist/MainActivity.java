6ce176fd1d46a2812f2ef108e8a9164d383007e9
Fatih İnan <fatihinan37@hotmail.com>
2013-11-28 21:43:32 +0200

    Yapılacak işlerin ekleneceği arayüz oluşturuldu.
    Yapılacak işlerin ekleneceği arayüz oluşturulmuştur. Takvim üzerinde bir
    güne tıklandığında bu arayüze geçiş yapması sağlanmıştır.

--- TODOLIST/src/com/example/todolist/MainActivity.java
+++ TODOLIST/src/com/example/todolist/MainActivity.java
@@ -1,16 +1,45 @@
 package com.example.todolist;
 
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.widget.CalendarView;
+import android.widget.CalendarView.OnDateChangeListener;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	
+	CalendarView takvim;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		takvim = (CalendarView) findViewById(R.id.takvim);
+		
+		takvim.setOnDateChangeListener(new OnDateChangeListener(){
+			 
+			@Override
+			public void onSelectedDayChange(CalendarView view,
+			int year, int month, int dayOfMonth) {
+			Toast.makeText(getApplicationContext(),
+			dayOfMonth +"/"+month+"/"+ year,Toast.LENGTH_LONG).show();
+			
+	    	startActivity(new Intent("android.intent.action.YAPILACAK_LISTESI"));
+			
+			
+			}
+		
+		});
+	
 	}
+			
+		
+		
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
---
+++ TODOLIST/src/com/example/todolist/yapilacak_listesi.java
@@ -0,0 +1,20 @@
+package com.example.todolist;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class yapilacak_listesi extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.yapilacak_listesi);
+
+	}
+	
+	
+	
+
+}
